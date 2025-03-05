9259703818267b3c53812458331b18e4c0b9e166
Carlos Gutierrez <devcarlosg@gmail.com>
2025-03-05 01:20:11 -0400

    feat: add aut.controller.ts, auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import * as bcrypt from 'bcryptjs';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  ConflictException,
+} from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { LoginDTO, LoginResponseDTO } from './dto/login.dto';
+import { SignUpDTO } from './dto/signUp.dto';
+import { UserDTO } from 'src/user/dto/users.dto';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
@@ -24,5 +30,31 @@ export class AuthService {
     return {
       accessToken: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async signUp(signUpDTO: SignUpDTO) {
+    const existingUser = await this.userService.findByEmail(signUpDTO.email);
+    if (existingUser) {
+      throw new ConflictException('The email is already in use');
+    }
+
+    const hashedPassword = await bcrypt.hash(signUpDTO.password, 10);
+
+    const newUserData = {
+      ...signUpDTO,
+      password: hashedPassword,
+    };
+
+    const newUser = await this.userService.create(newUserData);
+
+    const userCreated: UserDTO = {
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+      email: newUser.email,
+      documentId: newUser.documentId,
+      password: '',
+      phoneNumber: newUser.phoneNumber,
+    };
+    return userCreated;
   }
 }
---
+++ src/user/dto/users.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class UserDTO {
+  @ApiProperty({ description: 'The name of the user' })
+  @IsNotEmpty()
+  firstName: string;
+
+  @ApiProperty({ description: 'The last name of the user' })
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty({ description: 'The email of the user', uniqueItems: true })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ description: 'the password of the user' })
+  @IsNotEmpty()
+  password: string;
+
+  @ApiProperty({ description: 'the id of the user', uniqueItems: true })
+  @IsNotEmpty()
+  documentId: string;
+
+  @ApiProperty({ description: 'the phone number of the user' })
+  @IsNotEmpty()
+  @MinLength(13)
+  phoneNumber: string;
+}
