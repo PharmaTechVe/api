f752ab5b2899abd129b321f1396f8229951dfa7b
Andres Alvarez <andres15alvarez@gmail.com>
2025-03-09 17:47:39 -0400

    Delete useless comment

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDTO, LoginResponseDTO } from './dto/login.dto';
-//import { SignUpDTO } from './dto/sign-up.dto';
 import { UserDTO } from 'src/user/dto/user.dto';
 import { ApiResponse } from '@nestjs/swagger';
 
