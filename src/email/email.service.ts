664ff7daba11e99bdcb1c7d3b835888851f41816
Andres Alvarez <andres15alvarez@gmail.com>
2025-03-10 13:48:47 -0400

    Create service to send email

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ResendHelper } from './resend/resend.helper';
+import { EmailOptions, EmailOptionsContent } from './email.helper';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  constructor(private resend: ResendHelper) {}
+
+  async sendEmail(
+    options: EmailOptions & EmailOptionsContent,
+  ): Promise<boolean> {
+    return this.resend.sendEmail(options);
+  }
+}
