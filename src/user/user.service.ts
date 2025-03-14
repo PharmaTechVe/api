38c085e674b94710f7a5a1de46b33c77aa2546f5
Abraham <1001.28021547.ucla@gmail.com>
2025-03-14 15:49:44 -0400

    Remove unused function

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -19,9 +19,4 @@ export class UserService {
     const newUser = this.userRepository.create(userData);
     return await this.userRepository.save(newUser);
   }
-
-  async getUserRole(userId: string): Promise<string | null> {
-    const user = await this.userRepository.findOne({ where: { id: userId } });
-    return user ? user.role : null;
-  }
 }
