5f25e46533895e60b226fed75a6a69918e3b2396
Carlos Gutierrez <devcarlosg@gmail.com>
2025-03-17 23:08:30 -0400

    profileDTO extends UserDTO

--- src/user/dto/profile.dto.ts
+++ src/user/dto/profile.dto.ts
@@ -1,47 +1,14 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
-import { IsDateString, IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
-import { UserGender } from '../entities/profile.entity';
+import { OmitType, ApiProperty } from '@nestjs/swagger';
+import { UserDTO } from './user.dto';
 
-export class ProfileDTO {
-  @ApiProperty({ description: 'The name of the user' })
-  @IsNotEmpty()
-  firstName: string;
-
-  @ApiProperty({ description: 'The last name of the user' })
-  @IsNotEmpty()
-  lastName: string;
-
-  @ApiProperty({ description: 'The email of the user', uniqueItems: true })
-  @Transform(({ value }: { value: string }) => value.trim())
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-
-  @ApiProperty({ description: 'the id of the user', uniqueItems: true })
-  @IsNotEmpty()
-  documentId: string;
-
-  @ApiProperty({ description: 'the phone number of the user' })
-  @IsNotEmpty()
-  phoneNumber: string;
-
-  @ApiProperty({ description: 'the birth date of the user' })
-  @Transform(
-    ({ value }: { value: string }) =>
-      new Date(value).toISOString().split('T')[0],
-  )
-  @IsNotEmpty()
-  @IsDateString(
-    { strict: true },
-    { message: 'birthDate must be a valid date in YYYY-MM-DD format' },
-  )
+export class ProfileDTO extends OmitType(UserDTO, [
+  'password',
+  'birthDate',
+] as const) {
+  @ApiProperty({
+    description: 'birthDate must be a valid date in YYYY-MM-DD format',
+  })
   birthDate: Date;
-
-  @ApiProperty({ description: 'the gender of the user' })
-  @IsNotEmpty()
-  @IsEnum(UserGender)
-  gender: UserGender;
 
   @ApiProperty({ description: 'URL of the profile picture', nullable: true })
   profilePicture?: string;
