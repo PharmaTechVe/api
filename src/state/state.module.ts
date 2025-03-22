8c9be90d56560307682b5a13ea45c86dcf0b5950
Andres Alvarez <andres15alvarez@gmail.com>
2025-03-21 23:49:26 -0400

    Add missing migration

--- src/state/state.module.ts
+++ src/state/state.module.ts
@@ -4,10 +4,12 @@ import { StateController } from './state.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { State } from './entities/state.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { CountryService } from 'src/country/country.service';
+import { Country } from 'src/country/entities/country.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([State]), AuthModule],
+  imports: [TypeOrmModule.forFeature([State, Country]), AuthModule],
   controllers: [StateController],
-  providers: [StateService],
+  providers: [StateService, CountryService],
 })
 export class StateModule {}
