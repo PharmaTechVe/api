d045333d2ee77f77aebcf0d697e8e17061d01cef
Andres Alvarez <andres15alvarez@gmail.com>
2025-03-26 14:54:29 -0400

    Create endpoints for inventory with their respecitve guard for security and decoradtor for swagger documentation

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -6,40 +6,89 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
-import { CreateInventoryDto } from './dto/create-inventory.dto';
-import { UpdateInventoryDto } from './dto/update-inventory.dto';
+import {
+  CreateInventoryDTO,
+  UpdateInventoryDTO,
+  ResponseInventoryDTO,
+} from './dto/inventory.dto';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { UserRole } from 'src/user/entities/user.entity';
+import { Roles } from 'src/auth/roles.decorador';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Inventory } from './entities/inventory.entity';
 
 @Controller('inventory')
 export class InventoryController {
   constructor(private readonly inventoryService: InventoryService) {}
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.BRANCH_ADMIN)
   @Post()
-  create(@Body() createInventoryDto: CreateInventoryDto) {
-    return this.inventoryService.create(createInventoryDto);
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create an inventory' })
+  @ApiResponse({
+    description: 'Successful inventory creation',
+    status: HttpStatus.CREATED,
+    type: ResponseInventoryDTO,
+  })
+  async create(
+    @Body() createInventoryDTO: CreateInventoryDTO,
+  ): Promise<ResponseInventoryDTO> {
+    return await this.inventoryService.create(createInventoryDTO);
   }
 
   @Get()
-  findAll() {
-    return this.inventoryService.findAll();
+  @ApiOperation({ summary: 'List all inventory' })
+  async findAll(): Promise<ResponseInventoryDTO[]> {
+    return await this.inventoryService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.inventoryService.findOne(+id);
+  @ApiOperation({ summary: 'Retrieve an inventory' })
+  @ApiResponse({
+    description: 'Successful inventory retrieved',
+    status: HttpStatus.OK,
+    type: ResponseInventoryDTO,
+  })
+  async findOne(@Param('id') id: string): Promise<ResponseInventoryDTO> {
+    return await this.inventoryService.findOne(id);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.BRANCH_ADMIN)
   @Patch(':id')
-  update(
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update inventory by ID' })
+  @ApiResponse({
+    description: 'Successful update of inventory',
+    status: HttpStatus.OK,
+    type: Inventory,
+  })
+  async update(
     @Param('id') id: string,
-    @Body() updateInventoryDto: UpdateInventoryDto,
+    @Body() updateInventoryDTO: UpdateInventoryDTO,
   ) {
-    return this.inventoryService.update(+id, updateInventoryDto);
+    return await this.inventoryService.update(id, updateInventoryDTO);
   }
 
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.BRANCH_ADMIN)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.inventoryService.remove(+id);
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete inventory by ID' })
+  @ApiResponse({
+    description: 'Successful delete of inventory',
+    status: HttpStatus.NO_CONTENT,
+    type: Inventory,
+  })
+  async remove(@Param('id') id: string): Promise<void> {
+    await this.inventoryService.remove(id);
   }
 }
