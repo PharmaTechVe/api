d315447c9bb7d5dcea083b73b14ef00bceb0367d
Abraham <1001.28021547.ucla@gmail.com>
2025-03-27 15:22:41 -0400

    add migration user_otp

---
+++ src/user/migrations/1743102860945-add-column-user-otp-migration.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddColumnUserOtpMigration1743102860945
+  implements MigrationInterface
+{
+  name = 'AddColumnUserOtpMigration1743102860945';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."user_otp_type_enum" AS ENUM('password-recovery', 'email-validation')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user_otp" ADD "type" "public"."user_otp_type_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user_otp" ADD CONSTRAINT "unique_otp_code_per_type" UNIQUE ("code", "type")`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user_otp" DROP CONSTRAINT "unique_otp_code_per_type"`,
+    );
+    await queryRunner.query(`ALTER TABLE "user_otp" DROP COLUMN "type"`);
+    await queryRunner.query(`DROP TYPE "public"."user_otp_type_enum"`);
+  }
+}
