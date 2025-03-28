1385507520419513023db200cd5fa7d91e09638a
Carlos Gutierrez <devcarlosg@gmail.com>
2025-03-27 23:18:31 -0400

    Fixing conflicts

---
+++ src/user/migrations/1742857997608-addcolums-userotp-migration.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddcolumsUserotpMigration1742857997608
+  implements MigrationInterface
+{
+  name = 'AddcolumsUserotpMigration1742857997608';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."user_otp_type_enum" AS ENUM('password-recovery', 'email-validation')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user_otp" ADD "type" "public"."user_otp_type_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user_otp" ADD CONSTRAINT "unique_otp_code_per_type" UNIQUE ("code", "type")`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user_otp" DROP CONSTRAINT "unique_otp_code_per_type"`,
+    );
+    await queryRunner.query(`ALTER TABLE "user_otp" DROP COLUMN "type"`);
+    await queryRunner.query(`DROP TYPE "public"."user_otp_type_enum"`);
+  }
+}
