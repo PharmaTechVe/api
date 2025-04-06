4c975981b0dc0f3b4866b3a4d6635139579b789d
Dazzlin00 <eliza5478@gmail.com>
2025-04-06 14:23:50 -0400

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { StateModule } from './state/state.module';
 import { CityModule } from './city/city.module';
 import { BranchModule } from './branch/branch.module';
 import { InventoryModule } from './inventory/inventory.module';
-import { DiscountModule } from './discount/discount.module'; // Se importa el módulo discount
+import { DiscountModule } from './discount/discount.module';
 
 @Module({
   imports: [
