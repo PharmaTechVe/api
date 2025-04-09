87502987116ecfdf73c3fcc4baf1b54cdf3782d7
Andres Alvarez <andres15alvarez@gmail.com>
2025-04-09 16:20:56 -0400

    Add missing decorators

---
+++ src/order/dto/order.ts
@@ -0,0 +1,68 @@
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+import { OrderType } from '../entities/order.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateOrderDetailDTO {
+  @ApiProperty({ description: 'ID of the product presentation' })
+  @IsString()
+  @IsUUID()
+  productPresentationId: string;
+
+  @ApiProperty({ description: 'Quantity of products' })
+  @IsString()
+  quantity: number;
+}
+
+export class CreateOrderDTO {
+  @ApiProperty({
+    description: 'Type of order (PICKUP or DELIVERY)',
+    enum: OrderType,
+  })
+  @IsEnum(OrderType)
+  type: OrderType;
+
+  @ApiProperty({ description: 'ID of the branch for pickup orders' })
+  @IsString()
+  @IsUUID()
+  @IsOptional()
+  branchId?: string;
+
+  @ApiProperty({ description: 'ID of the user address for delivery orders' })
+  @IsString()
+  @IsUUID()
+  @IsOptional()
+  userAddressId?: string;
+
+  @ApiProperty({
+    description: 'List of products in the order',
+    type: [CreateOrderDetailDTO],
+  })
+  @IsArray()
+  @ArrayNotEmpty()
+  products: CreateOrderDetailDTO[];
+
+  constructor(
+    type: OrderType,
+    products: CreateOrderDetailDTO[],
+    branchId?: string,
+    userAddressId?: string,
+  ) {
+    if (type === OrderType.PICKUP && !branchId) {
+      throw new Error('Branch ID is required for pickup orders');
+    }
+    if (type === OrderType.DELIVERY && !userAddressId) {
+      throw new Error('User address ID is required for delivery orders');
+    }
+    this.type = type;
+    this.branchId = branchId;
+    this.userAddressId = userAddressId;
+    this.products = products;
+  }
+}
