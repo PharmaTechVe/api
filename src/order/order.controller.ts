869a73e264840a0d22b502b6ad51831538826e23
Andres Alvarez <andres15alvarez@gmail.com>
2025-04-09 16:23:25 -0400

    Create endpoint to create order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,42 +1,30 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
+  UseGuards,
+  Req,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { OrderService } from './order.service';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { CreateOrderDTO } from './dto/order';
+import { AuthGuard, CustomRequest } from 'src/auth/auth.guard';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
+  @HttpCode(HttpStatus.CREATED)
+  @UseGuards(AuthGuard)
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderService.create(createOrderDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.orderService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.orderService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.orderService.update(+id, updateOrderDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.orderService.remove(+id);
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Create a new order',
+    description: 'Creates a new order for the authenticated user.',
+  })
+  create(@Req() req: CustomRequest, @Body() createOrderDTO: CreateOrderDTO) {
+    return this.orderService.create(req.user, createOrderDTO);
   }
 }
