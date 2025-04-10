b67209cf60c83f179a6d2fe5dfa801ee9d75b10c
Andres Alvarez <andres15alvarez@gmail.com>
2025-04-09 23:11:57 -0400

    Create pagination query dto

--- src/order/dto/order.ts
+++ src/order/dto/order.ts
@@ -10,9 +10,11 @@ import {
   Min,
   ValidateNested,
 } from 'class-validator';
-import { OrderType } from '../entities/order.entity';
+import { OrderStatus, OrderType } from '../entities/order.entity';
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
+import { PaginationQueryDTO } from 'src/utils/dto/pagination.dto';
+import { BaseDTO } from 'src/utils/dto/base.dto';
 
 export class CreateOrderDetailDTO {
   @ApiProperty({ description: 'ID of the product presentation' })
@@ -74,4 +76,58 @@ export class CreateOrderDTO {
     this.userAddressId = userAddressId;
     this.products = products;
   }
+}
+
+export class ResponseOrderDetailDTO {
+  @ApiProperty({ description: 'ID of the product presentation' })
+  @IsString()
+  @IsUUID()
+  id: string;
+
+  @ApiProperty({ description: 'Quantity of products' })
+  @IsInt()
+  @IsPositive()
+  quantity: number;
+
+  @ApiProperty({ description: 'Subtotal price of the order detail' })
+  subtotal: number;
+}
+
+export class ResponseOrderDTO extends BaseDTO {
+  @ApiProperty({ description: 'Type of order (PICKUP or DELIVERY)' })
+  type: OrderType;
+
+  @ApiProperty({ description: 'Status of the order' })
+  status: OrderStatus;
+
+  @ApiProperty({ description: 'Total price of the order' })
+  totalPrice: number;
+
+  @ApiProperty({
+    description: 'Products in the order',
+    type: [ResponseOrderDetailDTO],
+  })
+  details: ResponseOrderDetailDTO[];
+}
+
+export class OrderQueryDTO extends PaginationQueryDTO {
+  @ApiProperty({ description: 'ID of the user' })
+  @IsOptional()
+  @IsUUID()
+  userId?: string;
+
+  @ApiProperty({ description: 'ID of the branch' })
+  @IsOptional()
+  @IsUUID()
+  branchId?: string;
+
+  @ApiProperty({ description: 'Type of order (PICKUP or DELIVERY)' })
+  @IsOptional()
+  @IsEnum(OrderType)
+  type?: OrderType;
+
+  @ApiProperty({ description: 'Status of the order' })
+  @IsOptional()
+  @IsEnum(OrderStatus)
+  status?: OrderStatus;
 }
