7edde23a9fa9383a00a2f6c932df930b9b752118
Andres Alvarez <andres15alvarez@gmail.com>
2025-05-06 20:14:29 -0400

    Expose properties

--- src/payments/dto/payment-confirmation.dto.ts
+++ src/payments/dto/payment-confirmation.dto.ts
@@ -1,20 +1,25 @@
 import { ApiProperty, IntersectionType } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 import { IsString, IsUUID } from 'class-validator';
 import { BaseDTO } from 'src/utils/dto/base.dto';
 
 class PaymentConfirmationDTO {
+  @Expose()
   @ApiProperty()
   @IsString()
   bank: string;
 
+  @Expose()
   @ApiProperty()
   @IsString()
   reference: string;
 
+  @Expose()
   @ApiProperty()
   @IsString()
   documentId: string;
 
+  @Expose()
   @ApiProperty()
   @IsString()
   phoneNumber: string;
