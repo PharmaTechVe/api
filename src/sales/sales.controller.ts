42a03e46cfa3d1f1acba094a8dc7b6c105be74cb
Abraham <1001.28021547.ucla@gmail.com>
2025-05-11 18:01:34 -0400

    restrict endpoint access to ADMIN and BRANCH_ADMIN roles

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { SalesService } from './sales.service';
 import { PredictedSaleDTO, PredictSalesDTO } from './dto/predict-sales.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Roles } from 'src/auth/roles.decorador';
+import { UserRole } from 'src/user/entities/user.entity';
 
 @Controller('sales')
 export class SalesController {
   constructor(private readonly salesService: SalesService) {}
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.BRANCH_ADMIN)
   @Get('predict')
   async predict(@Query() query: PredictSalesDTO): Promise<PredictedSaleDTO[]> {
     const salesData = await this.salesService.getDailySales();
--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from 'src/order/entities/order.entity';
 import { SalesController } from './sales.controller';
 import { SalesService } from './sales.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [TypeOrmModule.forFeature([Order]), AuthModule],
   controllers: [SalesController],
   providers: [SalesService],
 })
